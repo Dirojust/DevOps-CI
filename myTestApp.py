b298e31e1d8726ae18953b4e8dfe7e26ac6cb25c
diana.rojas <diana.rojas@jalasoft.com>
2025-10-06 10:25:47 -0400

    logs - improved

--- myTestApp.py
+++ myTestApp.py
@@ -1,9 +1,14 @@
 from flask import Flask
+import logging
+
 app = Flask(__name__)
 
-@app.route("/")
+logging.basicConfig(level=logging.DEBUG)
+
+@app.route("/saludo")
 def hello():
+    app.logger.debug("Ruta Saludo fue llamada")
     return "Hola desde Bolivia!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
